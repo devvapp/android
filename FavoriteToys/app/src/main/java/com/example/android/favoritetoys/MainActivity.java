705d369895907cbff89f2319a8413f4440ab0ffe
Aneesh Kolloju <AneeshChari.Kolloju@searshc.com>
2018-02-10 01:03:28 -0500

    Committing action menu search with a toast message

--- FavoriteToys/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.android.favoritetoys;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +33,18 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int menuItemThatWasSelected = item.getItemId();
+        if(menuItemThatWasSelected == R.id.action_search){
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
