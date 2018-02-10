99ca8dfe2e5c9e4e90cac29d0ebf582083043458
Aneesh Kolloju <AneeshChari.Kolloju@searshc.com>
2018-02-10 01:50:02 -0500

    Connection github serach query app to internet

--- FavoriteToys/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/com/example/android/favoritetoys/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.android.favoritetoys;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.android.favoritetoys.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,12 +45,49 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         int menuItemThatWasSelected = item.getItemId();
-        if(menuItemThatWasSelected == R.id.action_search){
+        if (menuItemThatWasSelected == R.id.action_search) {
             Context context = MainActivity.this;
             String message = "Search clicked";
             Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+            this.makeGithubSearchQuery();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private void makeGithubSearchQuery() {
+        String githubQuery = mSearchBoxEditText.getText().toString();
+        URL url = NetworkUtils.buildUrl(githubQuery);
+        mUrlDisplayTextView.setText(url.toString());
+        new GithubQueryTask().execute(url);
+    }
+
+
+    /**
+     * Make aynchronous call to get the data from github
+     */
+    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+        @Override
+        protected String doInBackground(URL... urls) {
+
+            URL url = urls[0];
+            String githubSearchResults = null;
+
+            try {
+                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return githubSearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s != null && !s.equals("")){
+                mSearchResults.setText(s);
+            }
+        }
     }
 }
