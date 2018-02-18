8b17ef802ce49e752f3e023ff2aede75001e7c7b
devvapp <devvapp@gmail.com>
2018-02-18 03:17:45 -0600

    Adding new detail activity, using intents and sharing

--- Sunshine/app/src/main/java/com/devvapp/play/sunshine/MainActivity.java
+++ Sunshine/app/src/main/java/com/devvapp/play/sunshine/MainActivity.java
@@ -1,6 +1,7 @@
 package com.devvapp.play.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -149,12 +150,14 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapter.F
 
     @Override
     public void onClick(String weatherForDay) {
-        Context context = this;
         // TODO (1) Create a new Activity called DetailActivity using Android Studio's wizard
         // TODO (2) Change the root layout of activity_detail.xml to a FrameLayout and remove unnecessary xml attributes
         // TODO (3) Remove the Toast and launch the DetailActivity using an explicit Intent
-        Toast.makeText(context, weatherForDay, Toast.LENGTH_SHORT)
-                .show();
+        Context context = this;
+        Class destinationClass = DetailActivity.class;
+        Intent intentToStartDetailActivity = new Intent(context, destinationClass);
+        intentToStartDetailActivity.putExtra(Intent.EXTRA_TEXT, weatherForDay);
+        startActivity(intentToStartDetailActivity);
     }
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
